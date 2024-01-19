5eaf9f471fb9c87f21df92e8d94d1a7c7b1e5935
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-19 14:40:19 +0100

    refacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import Home from "./components/Accueil/Home";
-import Presentation from "./components/Presentation/Presentation";
-import Map from "./components/Carte/Map";
-import Localization from "./components/Localisation/Localization";
+import Home from "./pages/Accueil/Home";
+import Presentation from "./pages/Presentation/Presentation";
+import Map from "./pages/Carte/Map";
+import Localization from "./pages/Localisation/Localization";
+import Footer from "./pages/Footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/presentation" component={Presentation} />
-        <Route path="/carte" component={Map} />
-        <Route path="/localisation" component={Localization} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/presentation" element={<Presentation />} />
+        <Route path="/carte" element={<Map />} />
+        <Route path="/localisation" element={<Localization />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
