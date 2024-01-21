e21485b7bec6a5b39a8d692eec39940539d1c069
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-21 13:16:49 +0100

    Create index.jsx

---
+++ src/models/utils/context/index.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import { useFetchData } from "../hooks";
+
+export const ListePlatContext = createContext();
+
+export default function ListePlatProvider({ children }) {
+  const { data } = useFetchData("https://dummyjson.com/recipes");
+
+  const [ListePlatState, setListePlat] = useState([]);
+
+  useEffect(() => {
+    setListePlat(data.recipes)
+    debugger
+  }, [data]);
+
+
+  return (
+    <ListePlatContext.Provider
+      value={{
+        ListePlatState
+      }}
+    >
+      {children}
+    </ListePlatContext.Provider>
+  );
+}
