c40de4354dd8ec3ae99b2ed4c20f7876b0cd234f
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-21 13:17:03 +0100

    Create ListePlat.jsx

---
+++ src/components/ListePlat/ListePlat.jsx
@@ -0,0 +1,14 @@
+import './ListePlat.css'
+import Plat from "../Plat"
+
+export default function ListePlat({listePlat}){
+    
+    return (
+        <div className="list-plat">
+        {
+          listePlat &&
+          listePlat.map((plat) => <Plat key={plat.id} plat={plat} />)
+        }
+        </div>
+    )
+}
