6faa13fd0c22230b27c77687a8b8dd1cd15dd253
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-21 13:17:38 +0100

    Update Home.jsx

--- src/pages/Accueil/Home.jsx
+++ src/pages/Accueil/Home.jsx
@@ -1,10 +1,21 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 function Home() {
   return (
     <div>
       <h1>Accueil</h1>
+
+      <div className="demo-carousel">
+        <Carousel>
+          <div>
+            <img src="https://picsum.photos/1200/400?img=1" />
+            <p className="legend">Legend 1</p>
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
 }
