332f4069f36e93a9c11a74d4000b54d2f18a9a41
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-21 13:17:31 +0100

    Patch app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Accueil/Home";
@@ -8,11 +8,24 @@ import Map from "./pages/Carte/Map";
 import Localization from "./pages/Localisation/Localization";
 import Footer from "./pages/Footer/Footer";
 import Header from "./pages/Header/Header";
+import ListePlatProvider from "./models/utils/context";
+
+import ListePlat from "./components/ListePlat/ListePlat";
+import { useFetchData } from "./models/utils/hooks";
+import { useEffect } from "react";
 
 function App() {
+  const { data } = useFetchData("https://dummyjson.com/recipes");
+
+  const [platState, setPlatState] = useState([]);
+
+  useEffect(() => {
+    setPlatState(data.recipes)
+  }, [data]);
+
   return (
     <>
-      <Header />  
+      <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -20,6 +33,8 @@ function App() {
         <Route path="/carte" element={<Map />} />
         <Route path="/localisation" element={<Localization />} />
       </Routes>
+
+      <ListePlat listePlat={platState} />
 
       <Footer />
     </>
