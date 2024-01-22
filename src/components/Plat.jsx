71c518a4563f56423593bf35582e2b822abbc3dc
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-22 12:09:37 +0100

    Update Plat.jsx

--- src/components/Plat.jsx
+++ src/components/Plat.jsx
@@ -1,16 +1,13 @@
 import React, { Component, useEffect, useState } from "react";
-import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 
 function Plat({ plat }) {
-    return (
-      <div className="plat-item">
-        <img src={plat.image} alt={plat.name} />
-        <p>{plat.name}</p>
-      </div>
-    );
-  }
-  
-  export default Plat;
-  
+  return (
+    <div className="plat-item">
+      <img src={plat.image} alt={plat.name} />
+      <p>{plat.name}</p>
+    </div>
+  );
+}
+
+export default Plat;
