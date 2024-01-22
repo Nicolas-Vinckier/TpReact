cb8894c668eb067168c6a3b3665154326c8741e9
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-22 10:01:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Accueil/Home";
 import Presentation from "./pages/Presentation/Presentation";
-import Map from "./pages/Carte/Map";
+import Map from "./pages/Carte/Menu";
 import Localization from "./pages/Localisation/Localization";
 import Footer from "./pages/Footer/Footer";
 import Header from "./pages/Header/Header";
-import ListePlatProvider from "./models/utils/context";
 
-import ListePlat from "./components/ListePlat/ListePlat";
-import { useFetchData } from "./models/utils/hooks";
-import { useEffect } from "react";
+// import ListePlatProvider from "./models/utils/context";
+// import ListePlat from "./components/ListePlat/ListePlat";
+// import { useFetchData } from "./models/utils/hooks";
+// import { useEffect } from "react";
 
 function App() {
-  const { data } = useFetchData("https://dummyjson.com/recipes");
+  // const { data } = useFetchData("https://dummyjson.com/recipes");
 
-  const [platState, setPlatState] = useState([]);
+  // const [platState, setPlatState] = useState([]);
 
-  useEffect(() => {
-    setPlatState(data.recipes);
-  }, [data]);
+  // useEffect(() => {
+  //   setPlatState(data.recipes);
+  // }, [data]);
 
   return (
     <>
@@ -33,8 +33,6 @@ function App() {
         <Route path="/carte" element={<Map />} />
         <Route path="/localisation" element={<Localization />} />
       </Routes>
-
-      <ListePlat listePlat={platState} />
 
       <Footer />
     </>
