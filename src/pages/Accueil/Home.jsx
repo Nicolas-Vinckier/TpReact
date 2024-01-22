3ab61f6932cb16ea784c4c9d8e08f89a0d4e7409
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-22 10:21:41 +0100

    Update Home.jsx

--- src/pages/Accueil/Home.jsx
+++ src/pages/Accueil/Home.jsx
@@ -1,11 +1,7 @@
-import React, { Component, useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
-import Plat from "../../components/Plat";
-import ListePlat from "../../components/ListePlat/ListePlat";
-import { ListePlatContext } from "../../models/utils/context";
 import { useFetchData } from "../../models/utils/hooks";
 
 function Home() {
@@ -20,7 +16,7 @@ function Home() {
     <div>
       <h1>Accueil</h1>
 
-      <div className="demo-carousel">
+      <div className="carousel">
         <Carousel>
           {platState.map((plat) => (
             <div key={plat.id}>
